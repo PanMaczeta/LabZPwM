bea26e027ca0c2cba1f99eb61ea54654225392a2
PanMaczeta <52625108+PanMaczeta@users.noreply.github.com>
2020-03-24 19:00:28 +0100

    ico bez kodu

---
+++ LAB3/source/res.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by res.rc
+//
+#define IDD_MAINVIEW                    101
+#define IDI_MAINICON1                   102
+#define IDI_MAINICONSMALL1              103
+#define IDI_MAINICON02                  104
+#define IDI_MAINICONSMALL02             105
+#define IDI_ICON1                       106
+#define IDI_MAINICON                    106
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
